60b40f29f03bc448fcac879cf8a0f65b77aafcdf
matcodesalot <reallymathew@gmail.com>
2016-08-21 15:49:21 -0500

    ryu-cool

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 		//alert("mouse entered .ryu div");
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
+		$(".ryu-cool").hide();
 	})
 	.mouseleave(function() {
 		//alert("bye bye mousey");
 		$(".ryu-still").show();
 		$(".ryu-ready").hide();
+		$(".ryu-cool").hide();
 	})
 	.mousedown(function() {
 		//console.log("the mouse has been clicked inside the .ryu div");
@@ -25,7 +27,24 @@ $(document).ready(function() {
 		//console.log("mouseup");
 		$(".ryu-ready").show();
 		$(".ryu-throwing").hide();
+	});
+
+	$(document).keydown(function(event) {
+		if(event.which == 88) {
+			//console.log("the x key has been pressed");
+			$(".ryu-cool").show();
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+		}
 	})
+	.keyup(function(event) {
+		if(event.which == 88) {
+			//console.log("we released the x key");
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+			$(".ryu-ready").hide();
+		}
+	});
 });
 
 function playHadouken() {
